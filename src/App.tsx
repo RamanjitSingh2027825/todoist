429a3f26f4500722acbc89712eea294886481f62
Ramanjit Singh <ramanbainsedu214@gmail.com>
2025-02-24 11:10:47 +0530

    add initial components and initial styles for Todo application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
+import TodoInput from "./components/TodoInput/TodoInput";
+
+import styles from "./styles/app.module.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.appContainer}>
+        <Header />
+        <TodoList />
+        <TodoInput />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import App from "./App.tsx";
+
+import "modern-normalize/modern-normalize.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
